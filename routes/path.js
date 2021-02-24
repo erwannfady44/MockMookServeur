959f3b07cfeaecec812d2cdef86370068462a7ff
E194482T <EF2646cookie>
2021-02-24 14:43:34 +0100

    correction route

--- routes/path.js
+++ routes/path.js
@@ -11,7 +11,7 @@ router.delete('/:idPath', auth, pathController.delete);
 router.put('/:idPath/module', auth, pathController.addModule);
 router.post('/:idPath/:idModule', auth, pathController.editModule);
 router.delete('/:idPath/:idModule', auth, pathController.deleteModule);
-router.get('/:idPath/:idModule', auth, pathController.getOneModule);
+router.get('/:idPath/:idModule', pathController.getOneModule);
 router.get('/findByKeyWord', pathController.findByKeyWord);
 
 module.exports = router;
