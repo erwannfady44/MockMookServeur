0bc89eb0d9bb47181935f0cb87f784de5dc1ccf1
CocØ <corentin.pinguet@etu.univ-nantes.fr>
2021-02-26 11:38:51 +0100

    Fix resource : url non-unique

--- Models/Resource.js
+++ Models/Resource.js
@@ -4,7 +4,7 @@ const uniqueValidator = require('mongoose-unique-validator');
 const resourceSchema = mongoose.Schema({
     idModule: {type: mongoose.Schema.Types.ObjectId, ref: 'module', require: true},
     idCreator: {type: mongoose.Schema.Types.ObjectId, ref: 'User', require: true},
-    url: {type: String, require: true, unique: true},
+    url: {type: String, require: true},
     title: {type: String, require: true},
     description: {type: String, require: true},
     date: {type: Date, require: true},
