f02a8a86d67266832668460949d99d41b7446e41
CocØ <corentin.pinguet@etu.univ-nantes.fr>
2021-03-10 11:31:29 +0100

    Déplacement des fonctionnalités "Resource" du controller Path au controller Module

--- routes/module.js
+++ routes/module.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const moduleController = require('../controllers/module');
 const auth = require('../controllers/auth');
 
+router.put('/:idModule/resource', auth, moduleController.addResource);
+router.post('/:idModule/:idResource', auth, moduleController.editResource);
+router.get('/:idModule/:idResource', moduleController.getOneResource);
+router.delete('/:idModule/:idResource', auth, moduleController.deleteResource);
 router.put('/:idModule/:idResource/clone', auth, moduleController.cloneResource);
 router.get('/findByKeyWords', moduleController.findByKeyWord);
 
