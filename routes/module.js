dc0f5008dc7ddf4cf5ebd51eb2487d4e421e6ff4
E194482T <EF2646cookie>
2021-03-10 11:06:42 +0100

    correction commit de merde envoyé par CocØ

--- routes/module.js
+++ routes/module.js
@@ -3,15 +3,7 @@ const router = express.Router();
 const moduleController = require('../controllers/module');
 const auth = require('../controllers/auth');
 
-router.put('/:idModule', auth, moduleController.clone);
-router.get('/', moduleController.getAll);
 router.put('/:idModule/:idResource/clone', auth, moduleController.cloneResource);
 router.get('/findByKeyWords', moduleController.findByKeyWord);
-
-
-router.put('/:idModule/resource', auth, moduleController.addResource);
-router.post('/:idModule/:idResource', auth, moduleController.editResource);
-router.get('/:idModule/:idResource', moduleController.getOneResource);
-router.delete('/:idModule/:idResource', auth, moduleController.deleteResource);
 
 module.exports = router;
